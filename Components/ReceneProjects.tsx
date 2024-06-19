eb01b1dde8ab406c1d63429e1b8aadecba450b9b
prabashwara65 <prabashwara65@gmail.com>
2024-06-19 13:47:51 +0530

    added css

--- Components/ReceneProjects.tsx
+++ Components/ReceneProjects.tsx
@@ -7,16 +7,17 @@ import { FaLocationArrow } from 'react-icons/fa';
 
 const ReceneProjects = () => {
   return (
-    <div className='py-20'>
+    <div className='py-20' id='projects'>
       <h1 className='heading'>
         A small selection of{' '}
         <span className='text-purple'>Recent Projects</span>
       </h1>
       <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
         {projects.map(({ id, title, des, img, iconLists ,link }) => (
-          <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[90vw]'>
+          <div key={id} className=' sm:h-[41rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-96 w-[90vw]'>
             <PinContainer title={title} href={link}>
-              <div className='flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10 relative'>
+                {/* sm:w-[570px] */}
+              <div className='flex items-center justify-center sm:w-[400px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10 relative'>
                 <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                   <Image src='/bg.png' alt='bg-img' layout='fill' objectFit='cover' />
                 </div>
