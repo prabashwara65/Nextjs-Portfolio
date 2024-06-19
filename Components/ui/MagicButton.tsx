df0664402414b79b249f813b463fd52782a0a486
prabashwara65 <prabashwara65@gmail.com>
2024-06-19 08:57:58 +0530

    first commit

---
+++ Components/ui/MagicButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const MagicButton = ({
+    title , icon , posision , handleClick , otherClasses
+}: {
+    title : string; icon: React.ReactNode; posision: string; handleClick?: () => void; otherClasses?: string;
+}) => {
+    return (
+        <div>
+            <button className="p-[3px] relative">
+                <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+                <div className="px-9 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+                    {title}
+                </div>
+            </button>
+        </div>
+    )
+}
+
+export default MagicButton
